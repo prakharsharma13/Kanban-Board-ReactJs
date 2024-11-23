c38093291c6f112a153116567d95e342d342fd4f
prakharsharma13 <115884239+prakharsharma13@users.noreply.github.com>
2024-11-23 22:40:08 +0530

    building a basic task card

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,18 @@
 import "./App.css";
 
 function App() {
-  return <div>Hello</div>;
+  const title = "Do Market Research";
+  const id = "Bus 1";
+  const points = 5;
+  return (
+    <div className=" rounded-lg px-2 p-2 m-2 bg-gray-50">
+      <div className="text-base font-semibold py-2">{title}</div>
+      <div className="flex gap-4 justify-between text-gray-700">
+        <div>{id}</div>
+        <div>{points}</div>
+      </div>
+    </div>
+  );
 }
 
 export default App;
